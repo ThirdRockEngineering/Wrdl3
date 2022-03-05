46b1d3a65aace70f99b5df4bbf7e26a8f780546f
emil <poolsar.42@gmail.com>
2022-03-05 20:28:53 +0300

    pubsub start

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, UseEffect, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import SimpleStorageContract from "./contracts/SimpleStorage.json";
 import getWeb3 from "./getWeb3";
 import Node from "./ipfs";
@@ -33,7 +33,7 @@ const App = (props) => {
 
         // Get ipfs node instance
 
-        // const node = await Node;
+        const node = await Node;
 
         // Use web3 to get the user's accounts.
         const accounts = await web3.eth.getAccounts();
@@ -45,10 +45,18 @@ const App = (props) => {
           SimpleStorageContract.abi,
           deployedNetwork && deployedNetwork.address
         );
+
+        await node.pubsub.subscribe("main-room", () => {});
+        const peerIds = await node.pubsub.peers("main-room");
+        console.log("From pubsub stuff:", peerIds);
+
+        const topics = await node.pubsub.ls();
+        console.log("Topics you are subscribed to:", topics);
+
         setWeb3(web3);
         setAccounts(accounts);
         setContract(instance);
-        // setIpfsNode(node);
+        setIpfsNode(node);
       } catch (error) {
         // Catch any errors for any of the above operations.
         alert(
